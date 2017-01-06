425ce525a5f622604b5aa6edb08814e71a4ad5fd
dance2die <dontbothermewithspam@gmail.com>
2017-01-06 05:40:41 -0500

    Added a template

--- src/posts/views.py
+++ src/posts/views.py
@@ -4,8 +4,20 @@ from django.shortcuts import render
 # Create your views here.
 
 
-def post_home(request):
-    return HttpResponse('<h1>hello world</h1>')
+def post_create(request):
+    return HttpResponse('<h1>create</h1>')
+
+def post_detail(request):
+    return HttpResponse('<h1>detail</h1>')
+
+def post_list(request):
+    return render(request, 'index.html', {})
+
+def post_update(request):
+    return HttpResponse('<h1>update</h1>')
+
+def post_delete(request):
+    return HttpResponse('<h1>delete</h1>')
 
 
 
