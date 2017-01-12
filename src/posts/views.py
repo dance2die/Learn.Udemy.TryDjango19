e3a35d5c9d319c366b0f3516be3d91bad9e7d7d0
dance2die <dontbothermewithspam@gmail.com>
2017-01-12 06:59:25 -0500

    Added a new templated, post_detail

--- src/posts/views.py
+++ src/posts/views.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+from .models import Post
 
 # Create your views here.
 
@@ -7,14 +9,38 @@ from django.shortcuts import render
 def post_create(request):
     return HttpResponse('<h1>create</h1>')
 
+
 def post_detail(request):
-    return HttpResponse('<h1>detail</h1>')
+    instance = get_object_or_404(Post, id=3)
+    context = {
+        "title": instance.title,
+        "instance": instance
+    }
+    return render(request, 'post_detail.html', context)
+
 
 def post_list(request):
-    return render(request, 'index.html', {})
+    queryset = Post.objects.all()
+
+    context = {
+        "object_list": queryset,
+        "title": "List"
+    }
+
+    # if request.user.is_authenticated():
+    #     context = {
+    #         "title": "My user list"
+    #     }
+    # else:
+    #     context = {
+    #         "title": "List"
+    #     }
+    return render(request, 'index.html', context)
+
 
 def post_update(request):
     return HttpResponse('<h1>update</h1>')
+
 
 def post_delete(request):
     return HttpResponse('<h1>delete</h1>')
